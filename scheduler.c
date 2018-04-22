58bb5c8d0f70f8578a6604451ea2295fb33129cf
Francisco Geiman Thiesen <fthiesen@MacBook-Air-de-Francisco.local>
2018-04-22 18:09:03 -0300

    Deixando os parenteses errados e adicionando um handler para o caso do realTime ter overlap

--- scheduler.c
+++ scheduler.c
@@ -5,60 +5,60 @@
 #include <string.h>
 #include <sys/wait.h>
 
-
 int occupied[60]; // array to see if a second is available for a new real-time process
 
 // vai precisar ter um trecho de memória compartilhada
-struct process{
+struct process {
     char* name;
     int type, priority, start, duration;
-}typedef struct process proc;
+} typedef struct process proc;
 
 
-void newRealTime(process p)
-{
-    
+void newRealTime(process p) {
+    // checking for any conflict with another realTime process
+    if(p.start + p.duration > 60) {
+        printf("Erro de overflow na criacao do processo de nome = %s\n", p.name);
+        exit(-1);
+    }
+    for(int i = proc.start; i < proc.start + proc.duration; ++i) {
+        if(occupied[i]) {
+            printf("Erro, processo de nome %s quer usar segundos ocupados\n", p.name);
+            exit(-2);
+        }
+    }
 }
 
-void newPriority(process p)
-{
+void newPriority(process p) {
 
 }
 
-void newRoundRobin(process p)
-{
+void newRoundRobin(process p) {
 
 }
 
-int main()
-{
-    while(1)  
-    {
+int main() {
+
+    while(1) {
         // checar se tem um novo processo na memoria compartilhada ( o interpretador vai mandar um processo a cada segunda...
-        if( newProcess )
-        {
+        if( newProcess ) {
             // extrair as informacoes do processo
-            proc nuevo;
-            nuevo.name = getName();
-            nuevo.type = getType();
+            proc newBastard;
+            newBastard.name = getName();
+            newBastard.type = getType();
 
-            if(nuevo.type == 1) // RoundRobin Process
-            {
-                newRoundRobin(nuevo);
+            if(newBastard.type == 1) {
+                newRoundRobin(newBastard);
             }
-            else if(nuevo.type == 2) // priority process
-            {
-                nuevo.priority = getPriority();
-                newPriority(nuevo);
+            else if(newBastard.type == 2) {
+                newBastard.priority = getPriority();
+                newPriority(newBastard);
             }
-            else if(nuevo.type == 3)
-            {
-                nuevo.start = getStart();
-                nuevo.duration = getDuration();
-                newRealTime(nuevo);
+            else if(newBastard.type == 3) {
+                newBastard.start = getStart();
+                newBastard.duration = getDuration();
+                newRealTime(newBastard);
             }
-            else
-            {
+            else {
                 printf("Processo de tipo invalido\n");
                 exit(-1);
             }
