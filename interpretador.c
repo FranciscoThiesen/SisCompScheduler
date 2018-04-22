24b99a490d831c60b418933ac59aaeba878e7d1d
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2018-04-22 18:43:02 -0300

    add command params to shmem

--- interpretador.c
+++ interpretador.c
@@ -8,31 +8,34 @@
 #include <stdlib.h>
 
 const int bufferLength = 256;
-const int maxWordSize = 30;
+const int maxInputSize = 30;
 
-int main()
-{
+int main() {
     FILE* pInput;
     char* line = NULL;
     size_t len = 0;
     ssize_t read;
 
+    int interpret_mem = shmget(0, sizeof(pid_t), IPC_CREAT | S_IRUSR | S_IRWXU);
+    pid_t *interpret_pid = (int*) shmat(interpret_mem, 0, 0);
+    *interpret_pid = getpid();
+
+    int scheduler_mem = shmget(1, sizeof(pid_t), IPC_CREAT | S_IRUSR | S_IRWXU);
+    pid_t *scheduler_pid = (int*) shmat(scheduler_mem, 0, 0);
+
     pInput = fopen("exec.txt", "r");
 
-    if(pInput == NULL)
-    {
+    if(pInput == NULL) {
         printf("Falha na leitura do arquivo de executaveis\n");
         exit(-1);
     }
 
-    while( (read = getline(&line, &len, pInput) ) != -1)
-    {
+    while( (read = getline(&line, &len, pInput) ) != -1) {
         char* sentence[4];
         int wordIndex = 0;
         
         char* word = strtok(line, " ");
-        while(word != NULL)
-        {
+        while(word != NULL) {
             sentence[wordIndex++] = word; 
             word = strtok(NULL, " ");
         }
@@ -44,18 +47,28 @@ int main()
         // o tamanho da sentenca
         // wordIndex == 2 -> roundRobin, wordIndex == 3 -> Prioridades, == 4 -> real-time
         
-        if(wordIndex == 2) // roundRobin
-        {
+        int programNameMem = shmget(2, maxInputSize*sizeof(char), IPC_CREAT | S_IRUSR | S_IRWXU);
+        char *programName = (char*) shmat(programNameMem, 0, 0);
+        programName = sentence[1];
 
-        }
-        else if(wordIndex == 3) // prioridade
-        {
+        if (wordIndex == 3) { // prioridade
+            int priorityMem = shmget(4, sizeof(int), IPC_CREAT | S_IRUSR | S_IRWXU);
+            int *priority;
+            priority = (int*) shmat(priorityMem, 0, 0);
 
+            char* priorityParam = sentence[2];
+            *priority = priorityParam[3] - '0';
         }
-        else // real-time
-        {
+        else if (wordIndex == 4) { // real-time
+            int realtimeMem = shmget(5, 2*sizeof(int), IPC_CREAT | S_IRUSR | S_IRWXU);
+            int *data = (int*) shmat(realtimeMem, 0, 0);
 
+            char* start = *(sentence[2] + 2);
+            char* duration = *(sentence[3] + 2);
+            *data = {atoi(start), atoi(duration)};
         }
+
+        kill(*scheduler_pid, SIGSTOP); // send scheduler a signal indicating a line was read
     }
 
     fclose(pInput);
