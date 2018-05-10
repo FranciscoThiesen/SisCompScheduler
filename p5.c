efc8c2ae772a33f18d4916c778a81260279bbe17
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2018-05-09 23:40:58 -0300

    acabou

---
+++ p5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    for(int i = 0; i < 10; ++i)
+    {
+        printf("P5 rodando %d\n", i);
+        fflush(stdout);
+        sleep(1);
+    }
+}
